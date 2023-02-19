8e46f497a13497bfcee3e94318f802dfd6e3e6d9
YablonskaTanya <111698588+YablonskaTanya@users.noreply.github.com>
2023-02-20 01:16:03 +0200

    added desicion task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galerryImage = document.querySelector(".gallery__item");
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+      `;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryCardsMarkup);
+
+galleryContainer.addEventListener("click", onPaletteContainerClick);
+
+function onPaletteContainerClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt",
+  });
+}
